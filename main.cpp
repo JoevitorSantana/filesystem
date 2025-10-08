153a367418dae6a5d236280ee936e76a791f5c2b
JoevitorSantana <joevitoralvessantana@gmail.com>
2025-10-08 19:39:35 -0300

    Implementação remoção e inserção de arquivos com inodes indiretos.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 #define MAX_ENDERECOS_INDIRETOS_DUPLO_INODE 5
 #define MAX_ENDERECOS_INDIRETOS_TRIPLO_INODE 5
 #define MAX_ENDERECOS_BLOCOS_LISTA_LIVRE 10
+#define MAX_NUM_BLOCOS_INDIRETO_SIMPLES 10
+#define MAX_NUM_BLOCOS_INDIRETO_DUPLO 25
+#define MAX_NUM_BLOCOS_INDIRETO_TRIPLO 125
 
 struct inode
 {
@@ -115,6 +118,33 @@ void inicializarInode(Inode &inode)
     inode.enderecosTriploIndireto = -1;
 }
 
+void inicializarInodeSimples(INodeIndiretoSimples &inodeSimples)
+{
+    inodeSimples.quantidadeEnderecos = 0;
+    for (int j = 0; j < MAX_ENDERECOS_INDIRETOS_SIMPLES_INODE; j++)
+    {
+        inodeSimples.enderecos[j] = -1;
+    }
+}
+
+void inicializarInodeDuplo(INodeIndiretoDuplo &inodeDuplo)
+{
+    inodeDuplo.quantidadeEnderecos = 0;
+    for (int j = 0; j < MAX_ENDERECOS_INDIRETOS_DUPLO_INODE; j++)
+    {
+        inodeDuplo.enderecos[j] = -1;
+    }
+}
+
+void inicializarInodeTriplo(INodeIndiretoTriplo &inodeTriplo)
+{
+    inodeTriplo.quantidadeEnderecos = 0;
+    for (int j = 0; j < MAX_ENDERECOS_INDIRETOS_TRIPLO_INODE; j++)
+    {
+        inodeTriplo.enderecos[j] = -1;
+    }
+}
+
 void inicializarDiretorio(Diretorio &diretorio)
 {
     diretorio.quantidadeEntradas = 0;
@@ -125,13 +155,21 @@ void inicializarDiretorio(Diretorio &diretorio)
     }
 }
 
+void inicializarBloco(Bloco &bloco)
+{
+    bloco.tipo = FREE;
+    inicializarDiretorio(bloco.diretorio);
+    inicializarInode(bloco.inode);
+    inicializarInodeSimples(bloco.inodeEnderecoSimples);
+    inicializarInodeDuplo(bloco.inodeEnderecoDuplo);
+    inicializarInodeTriplo(bloco.inodeEnderecoTriplo); 
+}
+
 void inicializarBlocos(Bloco *disco, int quantidadeBlocos)
 {
     for (int i = 0; i < quantidadeBlocos; i++)
     {
-        disco[i].tipo = FREE;
-        inicializarDiretorio(disco[i].diretorio);
-        inicializarInode(disco[i].inode);
+        inicializarBloco(disco[i]);
     }
 }
 
@@ -351,56 +389,141 @@ void inserirArquivo(char *nome, int tamanhoEmBytes, ListaBlocosLivres *lista, Bl
     disco[enderecoBlocoInode].inode.contadorHardLinks = 1;
 
     int quantidadeAlocada = 0;
+    int bloco;
+
+    INodeIndiretoSimples inodeSimples[MAX_ENDERECOS_INDIRETOS_SIMPLES_INODE];
+
+    INodeIndiretoSimples inodeSimplesListaIndiretoTriplo[MAX_NUM_BLOCOS_INDIRETO_DUPLO];
+    INodeIndiretoDuplo inodeDuploListaIndiretoTriplo[MAX_ENDERECOS_INDIRETOS_DUPLO_INODE];
+
+    INodeIndiretoDuplo inodeDuplo;
+    INodeIndiretoTriplo inodeTriplo;
+    
+    int qtdeInodeDuplo = 0, qtdeInodeSimples = 0;
+    int qtdeInodeDuploLIT = 0, qtdeInodeSimplesLIT = 0;
 
     while (quantidade > 0 && Aux != NULL) {
         // verificar se a pilha de blocos do no atual nao esta vazia
         while (quantidade > 0 && Count(Aux->blocos) > 0) {
-            //if (quantidadeAlocada < MAX_ENDERECOS_DIRETOS_INODE) {
-            int bloco = Pop(Aux->blocos);
-            disco[enderecoBlocoInode].inode.enderecosDiretos[quantidadeBlocos - quantidade] = bloco;
-            disco[bloco].tipo = ARQUIVO;
-                // printf("Bloco alocado: %d\n", bloco);
-            // } else if (quantidadeBlocos > MAX_ENDERECOS_DIRETOS_INODE && quantidadeBlocos <= (MAX_ENDERECOS_DIRETOS_INODE + MAX_ENDERECOS_INDIRETOS_SIMPLES_INODE))
-            // {   // determinar se vai usar bloco de enderecos simples, duplo ou triplo
-            //     // alocar bloco de enderecos simples
-            //     if (disco[enderecoBlocoInode].inode.enderecoSimplesIndireto == -1) {
-            //         int blocoEnderecoSimples = alocarBloco(lista);
-            //         disco[enderecoBlocoInode].inode.enderecoSimplesIndireto = blocoEnderecoSimples;
-            //         disco[blocoEnderecoSimples].tipo = INODE;
-            //         disco[blocoEnderecoSimples].inodeEnderecoSimples.quantidadeEnderecos = 0;
-            //     }
-            //     int bloco = Pop(Aux->blocos);
-            //     disco[disco[enderecoBlocoInode].inode.enderecoSimplesIndireto].inodeEnderecoSimples.enderecos[disco[disco[enderecoBlocoInode].inode.enderecoSimplesIndireto].inodeEnderecoSimples.quantidadeEnderecos++] = bloco;
-            //     disco[bloco].tipo = ARQUIVO;
-            // } else if (quantidadeBlocos > (MAX_ENDERECOS_DIRETOS_INODE + MAX_ENDERECOS_INDIRETOS_SIMPLES_INODE) && quantidadeBlocos < MAX_ENDERECOS_DIRETOS_INODE * MAX_ENDERECOS_INDIRETOS_DUPLO_INODE) 
-            // {   // Nó duplo indireto
-            //     // alocar bloco de enderecos duplos
-            //     if (disco[enderecoBlocoInode].inode.enderecosDuploIndireto == -1) {
-            //         int blocoEnderecoDuplo = alocarBloco(lista);
-            //         disco[enderecoBlocoInode].inode.enderecosDuploIndireto = blocoEnderecoDuplo;
-            //         disco[blocoEnderecoDuplo].tipo = INODE;
-            //         disco[blocoEnderecoDuplo].inodeEnderecoDuplo.quantidadeEnderecos = 0;
-            //     }
-            //     // alocar bloco de enderecos simples dentro do bloco de enderecos duplos
-            //     if (disco[disco[enderecoBlocoInode].inode.enderecosDuploIndireto].inodeEnderecoDuplo.quantidadeEnderecos == 0 || 
-            //         disco[disco[enderecoBlocoInode].inode.enderecosDuploIndireto].inodeEnderecoDuplo.enderecos[disco[disco[enderecoBlocoInode].inode.enderecosDuploIndireto].inodeEnderecoDuplo.quantidadeEnderecos - 1] == MAX_ENDERECOS_INDIRETOS_SIMPLES_INODE) 
-            //     {
-            //         int blocoEnderecoSimples = alocarBloco(lista);
-            //         disco[disco[enderecoBlocoInode].inode.enderecosDuploIndireto].inodeEnderecoDuplo.enderecos[disco[disco[enderecoBlocoInode].inode.enderecosDuploIndireto].inodeEnderecoDuplo.quantidadeEnderecos++] = blocoEnderecoSimples;
-            //         disco[blocoEnderecoSimples].tipo = INODE;
-            //         disco[blocoEnderecoSimples].inodeEnderecoSimples.quantidadeEnderecos = 0;
-            //     }
-            //     int bloco = Pop(Aux->blocos);
-            //     int blocoSimplesAtual = disco[disco[enderecoBlocoInode].inode.enderecosDuploIndireto].inodeEnderecoDuplo.enderecos[disco[disco[enderecoBlocoInode].inode.enderecosDuploIndireto].inodeEnderecoDuplo.quantidadeEnderecos - 1];
-            //     disco[blocoSimplesAtual].inodeEnderecoSimples.enderecos[disco[blocoSimplesAtual].inodeEnderecoSimples.quantidadeEnderecos++] = bloco;
-            //     disco[bloco].tipo = ARQUIVO;
-            // }
+            if (quantidadeAlocada < MAX_ENDERECOS_DIRETOS_INODE) {
+                bloco = alocarBloco(lista);
+                disco[enderecoBlocoInode].inode.enderecosDiretos[quantidadeBlocos - quantidade] = bloco;
+                disco[bloco].tipo = ARQUIVO;                
+            } else if (quantidadeBlocos >= MAX_ENDERECOS_DIRETOS_INODE && quantidadeBlocos <= MAX_NUM_BLOCOS_INDIRETO_SIMPLES)
+            {
+                if (quantidadeAlocada == MAX_ENDERECOS_DIRETOS_INODE) {
+                    // alocar um bloco para o inode de enderecos indiretos simples
+                    int blocoInodeSimples = alocarBloco(lista);
+                    disco[enderecoBlocoInode].inode.enderecoSimplesIndireto = blocoInodeSimples;
+                    disco[blocoInodeSimples].tipo = INODE;
+                }
+                
+                int posBlocoInodeIndiretoSimples = disco[enderecoBlocoInode].inode.enderecoSimplesIndireto;
+                bloco = Pop(Aux->blocos);
 
+                // inserir o bloco na lista de enderecos do inode de enderecos indiretos simples
+                disco[posBlocoInodeIndiretoSimples].inodeEnderecoSimples.enderecos[disco[posBlocoInodeIndiretoSimples].inodeEnderecoSimples.quantidadeEnderecos++] = bloco;
+                disco[bloco].tipo = ARQUIVO;
+            } else if (quantidadeBlocos > MAX_NUM_BLOCOS_INDIRETO_SIMPLES && quantidadeBlocos <= MAX_NUM_BLOCOS_INDIRETO_DUPLO)
+            {
+                if (quantidadeAlocada == MAX_ENDERECOS_DIRETOS_INODE) {
+                    // inicializar os inodos indiretos simples
+                    inicializarInodeDuplo(inodeDuplo);
+
+                    for (int i = 0; i < MAX_ENDERECOS_INDIRETOS_DUPLO_INODE; i++)
+                        inicializarInodeSimples(inodeSimples[i]);
+                }
+                
+                //  verificar a quantidade de inodes simples ja alocados
+                if (inodeSimples[qtdeInodeSimples].quantidadeEnderecos == MAX_ENDERECOS_INDIRETOS_SIMPLES_INODE)
+                    qtdeInodeSimples++;
+
+                bloco = alocarBloco(lista);
+                inodeSimples[qtdeInodeSimples].enderecos[inodeSimples[qtdeInodeSimples].quantidadeEnderecos++] = bloco;
+                disco[bloco].tipo = ARQUIVO;
+
+                if (quantidadeAlocada == quantidadeBlocos-1) {
+                    // alocar um bloco para o inode de enderecos indiretos duplos
+                    for (int i = 0; i <= qtdeInodeSimples; i++)
+                    {
+                        bloco = alocarBloco(lista);
+                        disco[bloco].inodeEnderecoSimples = inodeSimples[i];
+                        disco[bloco].tipo = INODE;
+
+                        inodeDuplo.enderecos[inodeDuplo.quantidadeEnderecos++] = bloco;
+                    }
+
+                    int blocoInodeDuplo = alocarBloco(lista);
+                    disco[enderecoBlocoInode].inode.enderecosDuploIndireto = blocoInodeDuplo;
+                    disco[blocoInodeDuplo].tipo = INODE;
+                    disco[blocoInodeDuplo].inodeEnderecoDuplo = inodeDuplo;
+                }
+            } else if (quantidadeBlocos > MAX_NUM_BLOCOS_INDIRETO_DUPLO && quantidadeBlocos <= MAX_NUM_BLOCOS_INDIRETO_TRIPLO)
+            {
+                if (quantidadeAlocada == MAX_ENDERECOS_DIRETOS_INODE) {
+                    inicializarInodeTriplo(inodeTriplo);
+
+                    // inializar os 5 inodes duplos
+                    for (int i = 0; i < MAX_ENDERECOS_INDIRETOS_DUPLO_INODE;i++)
+                    {
+                        inicializarInodeDuplo(inodeDuploListaIndiretoTriplo[i]);
+                    }
+                    
+                    // inicializar os 25 inodes simples
+                    for (int i = 0; i < MAX_NUM_BLOCOS_INDIRETO_DUPLO; i++)
+                    {
+                        inicializarInodeSimples(inodeSimplesListaIndiretoTriplo[i]);
+                    }
+                }
+
+                if (inodeSimplesListaIndiretoTriplo[qtdeInodeSimplesLIT].quantidadeEnderecos == MAX_ENDERECOS_INDIRETOS_DUPLO_INODE)
+                    qtdeInodeSimplesLIT++;
+                
+                bloco = alocarBloco(lista);
+                inodeSimplesListaIndiretoTriplo[qtdeInodeSimplesLIT].enderecos[inodeSimplesListaIndiretoTriplo[qtdeInodeSimplesLIT].quantidadeEnderecos++] = bloco;
+                disco[bloco].tipo = ARQUIVO;
+
+                if (quantidadeAlocada == quantidadeBlocos - 1)
+                {
+                    // percorrer os inodes simples qtdeInodeSimplesLIT
+                    // para cada um alocar um bloco para o inode duplo 
+                    for (int i = 0; i <= qtdeInodeSimplesLIT; i++)
+                    {
+                        bloco = alocarBloco(lista);
+                        disco[bloco].inodeEnderecoSimples = inodeSimplesListaIndiretoTriplo[i];
+                        disco[bloco].tipo = INODE;
+
+                        // adicionar ao nó pai
+                        inodeDuploListaIndiretoTriplo[qtdeInodeDuploLIT].enderecos[inodeDuploListaIndiretoTriplo[qtdeInodeDuploLIT].quantidadeEnderecos++] = bloco;
+
+                        if (inodeDuploListaIndiretoTriplo[qtdeInodeDuploLIT].quantidadeEnderecos == MAX_ENDERECOS_INDIRETOS_DUPLO_INODE)
+                            qtdeInodeDuploLIT++;
+                    }
+
+                    // percorrer os nós duplos alocar blocos e adicionar no noh triplo
+                    for (int i = 0; i <= qtdeInodeDuploLIT; i++)
+                    {
+                        bloco = alocarBloco(lista);
+                        disco[bloco].inodeEnderecoDuplo = inodeDuploListaIndiretoTriplo[i];
+                        disco[bloco].tipo = INODE;
+
+                        inodeTriplo.enderecos[inodeTriplo.quantidadeEnderecos++] = bloco;
+                    }
+
+                    bloco = alocarBloco(lista);
+                    disco[bloco].inodeEnderecoTriplo = inodeTriplo;
+                    disco[bloco].tipo = INODE;
+
+                    // adicionar ao inode principal
+                    disco[enderecoBlocoInode].inode.enderecosTriploIndireto = bloco;
+                }
+            }
+            
+            quantidadeAlocada++;
             quantidade--;
-            // quantidadeAlocada++;
         }
         // se a pilha de blocos do no atual estiver vazia, ir para o proximo no
-        if (Count(Aux->blocos) == 0){
+        if (quantidade > 0 && Count(Aux->blocos) == 0){
             RemoverInicio(lista);
             Aux = lista->cabeca;
         }
@@ -427,10 +550,93 @@ void removerArquivo(char *nome, int blocoAtual, Bloco *disco, ListaBlocosLivres 
     for (int j = 0; j < MAX_ENDERECOS_DIRETOS_INODE; j++) {
         if (disco[blocoInodeArquivo].inode.enderecosDiretos[j] != -1) {
             realocarBlocos(lista, disco[blocoInodeArquivo].inode.enderecosDiretos[j]);
-            disco[disco[blocoInodeArquivo].inode.enderecosDiretos[j]].tipo = FREE;
+            inicializarBloco(disco[disco[blocoInodeArquivo].inode.enderecosDiretos[j]]);
+            // disco[disco[blocoInodeArquivo].inode.enderecosDiretos[j]].tipo = FREE;
             disco[blocoInodeArquivo].inode.enderecosDiretos[j] = -1;
         }
     }
+
+    // liberar blocos inodes duplos
+    if (disco[blocoInodeArquivo].inode.enderecoSimplesIndireto != -1)
+    {
+        // percorrer os enderecos do bloco indireto simples realoando os blocs
+        int enderecoBlocoIndiretoSimples = disco[blocoInodeArquivo].inode.enderecoSimplesIndireto;
+
+        for (int i = 0; i < disco[enderecoBlocoIndiretoSimples].inodeEnderecoSimples.quantidadeEnderecos; i++)
+        {
+            int blocoRealocar = disco[enderecoBlocoIndiretoSimples].inodeEnderecoSimples.enderecos[i];
+            realocarBlocos(lista, blocoRealocar);
+            inicializarBloco(disco[blocoRealocar]);
+
+        }
+
+        realocarBlocos(lista, enderecoBlocoIndiretoSimples);
+        inicializarBloco(disco[enderecoBlocoIndiretoSimples]);
+        disco[blocoInodeArquivo].inode.enderecoSimplesIndireto = -1;
+    }
+
+    // remover de inode duplo indireto
+    if (disco[blocoInodeArquivo].inode.enderecosDuploIndireto != -1)
+    {
+        int enderecoBlocoIndiretoDuplo = disco[blocoInodeArquivo].inode.enderecosDuploIndireto;
+
+        for(int i = 0; i < disco[enderecoBlocoIndiretoDuplo].inodeEnderecoDuplo.quantidadeEnderecos; i++)
+        {
+            int enderecoBlocoIndiretoSimples = disco[enderecoBlocoIndiretoDuplo].inodeEnderecoDuplo.enderecos[i];
+
+            for (int j = 0; j < disco[enderecoBlocoIndiretoSimples].inodeEnderecoSimples.quantidadeEnderecos; j++)
+            {
+                int blocoRealocar = disco[enderecoBlocoIndiretoSimples].inodeEnderecoSimples.enderecos[j];
+                realocarBlocos(lista, blocoRealocar);
+                inicializarBloco(disco[blocoRealocar]);
+            }
+            
+            realocarBlocos(lista, enderecoBlocoIndiretoSimples);
+            inicializarBloco(disco[enderecoBlocoIndiretoSimples]);
+        }
+
+        realocarBlocos(lista, enderecoBlocoIndiretoDuplo);
+        inicializarBloco(disco[enderecoBlocoIndiretoDuplo]);
+        disco[blocoInodeArquivo].inode.enderecosDuploIndireto = -1;
+    }
+
+    // remover de inode triplo indireto
+    if (disco[blocoInodeArquivo].inode.enderecosTriploIndireto != -1)
+    {
+        int enderecoBlocoIndiretoTriplo = disco[blocoInodeArquivo].inode.enderecosTriploIndireto;
+
+        for (int i = 0; i < disco[enderecoBlocoIndiretoTriplo].inodeEnderecoTriplo.quantidadeEnderecos; i++)
+        {
+            int enderecoBlocoIndiretoDuplo = disco[enderecoBlocoIndiretoTriplo].inodeEnderecoTriplo.enderecos[i];
+
+            for(int i = 0; i < disco[enderecoBlocoIndiretoDuplo].inodeEnderecoDuplo.quantidadeEnderecos; i++)
+            {
+                int enderecoBlocoIndiretoSimples = disco[enderecoBlocoIndiretoDuplo].inodeEnderecoDuplo.enderecos[i];
+
+                for (int j = 0; j < disco[enderecoBlocoIndiretoSimples].inodeEnderecoSimples.quantidadeEnderecos; j++)
+                {
+                    int blocoRealocar = disco[enderecoBlocoIndiretoSimples].inodeEnderecoSimples.enderecos[j];
+                    realocarBlocos(lista, blocoRealocar);
+                    inicializarBloco(disco[blocoRealocar]);
+                }
+                
+                realocarBlocos(lista, enderecoBlocoIndiretoSimples);
+                inicializarBloco(disco[enderecoBlocoIndiretoSimples]);
+            }
+
+            realocarBlocos(lista, enderecoBlocoIndiretoDuplo);
+            inicializarBloco(disco[enderecoBlocoIndiretoDuplo]);
+            disco[blocoInodeArquivo].inode.enderecosDuploIndireto = -1;
+        }
+
+        realocarBlocos(lista, enderecoBlocoIndiretoTriplo);
+        inicializarBloco(disco[enderecoBlocoIndiretoTriplo]);
+        disco[blocoInodeArquivo].inode.enderecosTriploIndireto = -1;
+    }
+    // remover o inode do arquivo
+    realocarBlocos(lista, blocoInodeArquivo);
+    inicializarBloco(disco[blocoInodeArquivo]);
+
     // remover a entrada do diretorio    
     for (int j = i; j < disco[blocoAtual].diretorio.quantidadeEntradas - 1; j++) {
         disco[blocoAtual].diretorio.entradas[j] = disco[blocoAtual].diretorio.entradas[j + 1];
@@ -563,16 +769,16 @@ int main(void)
     // Alocrar um bloco para o inode
     // inserir o numero do bloco no inode e o nome do arquivo na entrada do diretório pai
     // Aloca os blocos do arquivo no inode
-    inserirArquivo("arquivo1.txt", 25, listaBlocosLivres, disco, blocoRaiz);
+    // inserirArquivo("arquivo1.txt", 1000, listaBlocosLivres, disco, blocoRaiz);
     // inserirArquivo("arquivo2.txt", 25, listaBlocosLivres, disco, blocoRaiz);
     // inserirArquivo("arquivo3.txt", 25, listaBlocosLivres, disco, blocoRaiz);
     // inserirArquivo("arquivo4.txt", 25, listaBlocosLivres, disco, blocoRaiz);
     // inserirArquivo("arquivo5.txt", 25, listaBlocosLivres, disco, blocoRaiz);
-    inserirDiretorio("diretorio1", listaBlocosLivres, disco, blocoRaiz);
+    //inserirDiretorio("diretorio1", listaBlocosLivres, disco, blocoRaiz);
     // abrir diretorio
-    int blocoAtual = abrirDiretorio("diretorio1", blocoRaiz, disco);
-    removerArquivo("arquivo1.txt", blocoRaiz, disco, listaBlocosLivres);
-    inserirArquivo("arquivo_diretorio1.txt", 150, listaBlocosLivres, disco, blocoAtual);
+    //int blocoAtual = abrirDiretorio("diretorio1", blocoRaiz, disco);
+    //removerArquivo("arquivo1.txt", blocoRaiz, disco, listaBlocosLivres);
+    //inserirArquivo("arquivo_diretorio1.txt", 150, listaBlocosLivres, disco, blocoAtual);
 
     // ###### FIM INSERCOES DE TESTE #######
     iniciarTerminal(disco, listaBlocosLivres);
